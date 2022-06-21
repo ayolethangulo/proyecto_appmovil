77f2c2a197c2a09484e3cf5dce2e807fae31a6b6
rleyb <56304806+rleyb@users.noreply.github.com>
2022-06-21 13:23:30 -0500

    Configuracion firebase
    Configuracion con firebase.
    Cambio de id aplication

--- android/app/src/main/kotlin/com/example/parcial_1_app_movil/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.parcial_1_app_movil
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/com/raitingRestaurant/proyecto_app_movil/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.raitingRestaurant.proyecto_app_movil
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
